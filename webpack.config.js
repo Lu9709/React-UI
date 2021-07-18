be71bd0a39d147dca66d2ce8e776e30b41b4a949
白褶 <919041098@qq.com>
2021-07-18 19:58:49 +0800

    webpack.config  prod/dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode:"production",
   resolve: {
     extensions: ['.ts','.tsx','.js','.jsx']
   },
@@ -22,25 +21,5 @@ module.exports = {
         loader: "awesome-typescript-loader"
       }
     ]
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-        title: "React-UI",
-        template: "index.html"
-      })
-  ],
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
   }
 }
