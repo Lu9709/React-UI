5eefaea46cfb4fc00ec50885a4c880b991cce041
白褶 <919041098@qq.com>
2021-07-19 15:57:52 +0800

    svg-sprite-loader

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button';
+import Icon from './icon';
 
-ReactDOM.render(<Button/>,document.body)
+ReactDOM.render(<div>
+  <Icon name="react"/>
+  <Icon name="github"/>
+  <Icon name="vue"/>
+</div>, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   resolve: {
     extensions: ['.ts','.tsx','.js','.jsx']
@@ -19,6 +18,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
       }
     ]
   }
